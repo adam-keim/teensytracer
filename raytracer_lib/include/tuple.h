2ef6f6d4a97dccfa9d79509f517a7f7a83e07f61
Adam Keim <adam.frusa@gmail.com>
2020-06-22 07:42:19 -0600

    Beginning to structure project

---
+++ raytracer_lib/include/tuple.h
@@ -0,0 +1,16 @@
+//
+// Created by Adam Keim on 6/22/20.
+//
+
+#ifndef TEENSYTRACER_TUPLE_H
+#define TEENSYTRACER_TUPLE_H
+
+
+class Tuple {
+public:
+    float x, y, z, w;
+    Tuple(float x, float y, float z, float w);
+};
+
+
+#endif //TEENSYTRACER_TUPLE_H
---
+++ raytracer_lib/tuple.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adam Keim on 6/22/20.
+//
+
+#include "include/tuple.h"
