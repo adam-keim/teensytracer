772415002be24572cc50c729f3d32c27676af3c0
Adam Keim <adam.frusa@gmail.com>
2020-06-29 13:19:48 -0600

    Still trying to get the eigen lib to work, still no luck

--- samples/ch2.cpp
+++ samples/ch2.cpp
@@ -33,23 +33,23 @@ void tick(Environment const &env, Projectile &proj) {
 
 int main() {
     Tuple start = Point(0,1,0);
-    Tuple vel = Vector(1, 1.8, 0).Normalize() * 11.25;
+    Tuple vel = Vector(1, 1.8, 0).normalized() * 11.25;
     Projectile p = Projectile(start, vel);
     Environment e = Environment(Vector(0, -.1, 0), Vector(-.01, 0, 0));
     Canvas c = Canvas(900, 550);
     do {
         tick(e, p);
         // Plot as 3x3 squares centered at pos
-        c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y-1), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y-1), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y-1), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y+1), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y+1), Color(1,1,0));
-        c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y+1), Color(1,1,0));
-    } while (p.m_position.y > 0);
+        c.WritePixel(int(p.m_position.x()-1), int(550 - p.m_position.y()-1), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()), int(550 - p.m_position.y()-1), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()+1), int(550 - p.m_position.y()-1), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()-1), int(550 - p.m_position.y()), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()), int(550 - p.m_position.y()), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()+1), int(550 - p.m_position.y()), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()-1), int(550 - p.m_position.y()+1), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()), int(550 - p.m_position.y()+1), Color(1,1,0));
+        c.WritePixel(int(p.m_position.x()+1), int(550 - p.m_position.y()+1), Color(1,1,0));
+    } while (p.m_position.y() > 0);
     std::ofstream out;
     out.open("scene.ppm");
     out << c;
--- teensy/main.cpp
+++ teensy/main.cpp
@@ -16,8 +16,10 @@
 
 #include <SPI.h>
 #include <TFT_ILI93XX.h>
-#include "include/tuple.h"
-#include "include/color.h"
+//#include "include/tuple.h"
+//#include "include/color.h"
+#include <Eigen/Dense>
+
 
 // For the Adafruit shield, these are the default.
 #define TFT_DC  9
@@ -26,28 +28,28 @@
 uint8_t errorCode = 0;
 
 TFT_ILI93XX tft = TFT_ILI93XX(TFT_CS, TFT_DC);
-
-struct Projectile {
-    Projectile(Tuple const &pos, Tuple const &vel) : m_position(pos), m_velocity(vel) {
-    }
-
-    Tuple m_position;
-    Tuple m_velocity;
-};
-
-struct Environment {
-    Environment(Tuple const &gravity, Tuple const &wind) : m_gravity(gravity), m_wind(wind) {
-    }
-
-    Tuple m_gravity;
-    Tuple m_wind;
-
-};
-
-void tick(Environment const &env, Projectile &proj) {
-    proj.m_position = proj.m_position + proj.m_velocity;
-    proj.m_velocity = proj.m_velocity + env.m_gravity + env.m_wind;
-}
+//
+//struct Projectile {
+//    Projectile(Tuple const &pos, Tuple const &vel) : m_position(pos), m_velocity(vel) {
+//    }
+//
+//    Tuple m_position;
+//    Tuple m_velocity;
+//};
+//
+//struct Environment {
+//    Environment(Tuple const &gravity, Tuple const &wind) : m_gravity(gravity), m_wind(wind) {
+//    }
+//
+//    Tuple m_gravity;
+//    Tuple m_wind;
+//
+//};
+//
+//void tick(Environment const &env, Projectile &proj) {
+//    proj.m_position = proj.m_position + proj.m_velocity;
+//    proj.m_velocity = proj.m_velocity + env.m_gravity + env.m_wind;
+//}
 
 
 void setup() {
@@ -76,23 +78,23 @@ void setup() {
 float loops = 0.0;
 
 void loop(void) {
-    Tuple start = Point(20, 20, 0);
-    Tuple vel = Vector(1 + loops, 1.8 - loops, loops).Normalize() * 11.25;
-    Projectile p = Projectile(start, vel);
-    Environment e = Environment(Vector(0, -.1, 0), Vector(-.01, 0, 0));
-    do {
-        tick(e, p);
-        tft.drawPixel(int(p.m_position.x), int(p.m_position.y), CYAN);
-        // Plot as 3x3 squares centered at pos
-//            c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y-1), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y-1), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y-1), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y+1), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y+1), Color(1,0,0));
-//            c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y+1), Color(1,0,0));
-    } while (p.m_position.y > 0);
-    loops += .05;
+//    Tuple start = Point(20, 20, 0);
+//    Tuple vel = Vector(1 + loops, 1.8 - loops, loops).normalized() * 11.25;
+//    Projectile p = Projectile(start, vel);
+//    Environment e = Environment(Vector(0, -.1, 0), Vector(-.01, 0, 0));
+//    do {
+//        tick(e, p);
+//        tft.drawPixel(int(p.m_position.x()), int(p.m_position.y()), CYAN);
+//        // Plot as 3x3 squares centered at pos
+////            c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y-1), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y-1), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y-1), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x-1), int(550 - p.m_position.y+1), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x), int(550 - p.m_position.y+1), Color(1,0,0));
+////            c.WritePixel(int(p.m_position.x+1), int(550 - p.m_position.y+1), Color(1,0,0));
+//    } while (p.m_position.y() > 0);
+//    loops += .05;
 }
